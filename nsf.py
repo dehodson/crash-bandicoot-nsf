4dd2e75b36d01b573d2c18907ef76926f0748d1a
Drew Hodson <dehodson@gmail.com>
2020-04-11 21:45:59 -0400

    Decode entry ids, start work on processing fields

--- nsf.py
+++ nsf.py
@@ -31,6 +31,35 @@ ENTRY_TYPES = {
 
 UNCOMPRESSED_CHUNK = b'3412'
 
+CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!";
+
+def eid_string(eid):
+    """Converts an integer EID to string"""
+    result = ""
+
+    if (eid & 0x80000000):
+        # If the high bit is set, this EID is not legitimate and cannot be
+        # converted to a 5-char string. Instead, we will prefix it with an
+        # equal sign. This symbol will be used when converting back from
+        # string to EID to indicate that the high bit is flipped.
+        result += '=';
+
+    result += CHARSET[(eid >> 25) & 0x3F];
+    result += CHARSET[(eid >> 19) & 0x3F];
+    result += CHARSET[(eid >> 13) & 0x3F];
+    result += CHARSET[(eid >> 7) & 0x3F];
+    result += CHARSET[(eid >> 1) & 0x3F];
+
+    if (~eid & 1):
+        # If the low bit is NOT set, this EID is not legitimate and cannot
+        # be converted to a 5-char string. Instead, we will append an equal
+        # sign to it. This symbol will be used when converting back from
+        # string to EID to indicate that the low bit is flipped.
+        result += '=';
+
+    return result
+
+
 class NsfFile:
     def __init__(self):
         self.chunks = []
@@ -78,7 +107,7 @@ class Entry:
         self.process()
 
     def __repr__(self):
-        string = 'Entry #{} - {}\n'.format(self.entry_id, ENTRY_TYPES[self.entry_type])
+        string = 'Entry {} - {}\n'.format(self.entry_id_string, ENTRY_TYPES[self.entry_type])
         for i in self.items:
             string += textwrap.indent(repr(i), '  ')
         return string
@@ -86,6 +115,7 @@ class Entry:
     def process(self):
         self.magic_number = self.raw_data[:4]
         self.entry_id = int.from_bytes(self.raw_data[4:8], byteorder='little')
+        self.entry_id_string = eid_string(self.entry_id)
         self.entry_type = int.from_bytes(self.raw_data[8:12], byteorder='little')
         self.item_count = int.from_bytes(self.raw_data[12:16], byteorder='little')
 
@@ -93,26 +123,82 @@ class Entry:
             self.item_offsets = []
 
             for i in range(self.item_count):
-                self.item_offsets.append(int.from_bytes(self.raw_data[16+(i*4):20+(i*4)], byteorder='little'))
+                self.item_offsets.append(int.from_bytes(self.raw_data[16+(i*4):20+(i*4)], byteorder='little', signed=False))
 
             self.items = []
-            for i in self.item_offsets:
-                item_length = int.from_bytes(self.raw_data[i:i+4], byteorder='little')
-                item = Item(self.raw_data[i:i+item_length])
+            for (i, v) in enumerate(self.item_offsets[:-1]):
+                if i < 2:
+                    continue # The format of the first two items is unknown. Not an entity
+                item_length = int.from_bytes(self.raw_data[v:v+4], byteorder='little', signed=True)
+                item = Item(self.raw_data[v:self.item_offsets[i+1]])
+                items_match = self.raw_data[v:self.item_offsets[i+1]] == item.serialize()
+                if not items_match:
+                    print('DEBUG: Non-matching item for entry:{}'.format(self.entry_id_string))
+                    print('DEBUG:', binascii.hexlify(self.raw_data[v:self.item_offsets[i+1]]))
+                    print('DEBUG:', binascii.hexlify(item.serialize()))
+                    print('DEBUG:---------------------------------------------------------------------- ')
                 self.items.append(item)
+
+
+FIELD_TYPES = {
+    0x2C: 'Name',
+    0x4B: 'Position',
+    0x9F: 'ID',
+    0xA4: 'General Settings',
+    0xA9: 'Type',
+    0xAA: 'Subtype',
+    0x103: 'SLST EID',
+    0x109: 'Neighbor camera rails (untested)',
+    0x118: 'Misc Settings 1',
+    0x13B: 'Draw List A',
+    0x13C: 'Draw List B',
+    0x208: 'Load List A',
+    0x209: 'Load List B',
+    0x277: 'Misc Settings 2',
+    0x287: 'Victim',
+    0x288: 'Section ID',
+    0x28B: 'Box count',
+    0x30E: 'Scale',
+    0x336: 'Time Trial crate type',
+    0x337: 'Bonus box count',
+}
+
+
+class Field:
+    def __init__(self, raw_field_info, raw_field_data):
+        self.raw_field_info = raw_field_info
+        self.raw_field_data = raw_field_data
+        self.process()
+
+    def __repr__(self):
+        return "Field of type: {}\n".format(FIELD_TYPES.get(self.field_type, 'Unknown ({})'.format(self.field_type)))
+
+    def process(self):
+        self.field_type = int.from_bytes(self.raw_field_info[:2], byteorder='little')
+
 
 class Item:
     def __init__(self, raw_data):
         self.raw_data = raw_data
+        self.fields = []
         self.process()
 
     def __repr__(self):
-        return 'Item "{}" - id:{} type:{} sub_type:{}\n'.format(self.name_string, self.item_id, self.item_type, self.sub_type)
+        string = 'Item "{}" - size:{} id:{} type:{} sub_type:{}\n'.format(self.name_string, self.item_length, self.item_id, self.item_type, self.sub_type)
+        for f in self.fields:
+            string += textwrap.indent(repr(f), '  ')
+        return string
 
     def process(self):
-        self.item_length = int.from_bytes(self.raw_data[:4], byteorder='little')
+        self.item_length = int.from_bytes(self.raw_data[:4], byteorder='little', signed=True)
         self.unused = self.raw_data[4:12]
-        self.field_count = int.from_bytes(self.raw_data[12:16], byteorder='little')
+        self.field_count = int.from_bytes(self.raw_data[12:16], byteorder='little', signed=True)
+        offset = 16
+
+        for i in range(self.field_count):
+            new_field = Field(self.raw_data[offset:offset+8], b'')
+            self.fields.append(new_field)
+            offset += 8
 
         offset = (8*self.field_count)+16
         self.field_data = self.raw_data[16:offset]
@@ -141,9 +227,9 @@ class Item:
 
     def serialize(self):
         binary_string = b''
-        binary_string += self.item_length.to_bytes(4, byteorder='little')
+        binary_string += self.item_length.to_bytes(4, byteorder='little', signed=True)
         binary_string += self.unused
-        binary_string += self.field_count.to_bytes(4, byteorder='little')
+        binary_string += self.field_count.to_bytes(4, byteorder='little', signed=True)
         binary_string += self.field_data
         binary_string += self.name_length.to_bytes(4, byteorder='little')
 
